aeab29389684215b26c8c3d73460f5e079ed856e
Terry-mcgit <13884967603@163.com>
2024-07-23 10:58:48 +0800

    test new js

--- js/transport.js
+++ js/transport.js
@@ -14,10 +14,10 @@ else{
 const userLang = navigator.language || navigator.userLanguage;
 const lanugages = ['cn','en','ru'];
 const basePath = '../src/md';
-    lanugages.forEach(lang => {
-        if (userLang.indexOf(`${lang}`) === 0) {
-            document.querySelectorAll(`[id^="content-${lang}"]`).forEach(function(div) {
-                div.style.display = 'block';
-            });
-        }
-    })
+lanugages.forEach(lang => {
+    if (userLang.indexOf(`${lang}`) === 0) {
+        document.querySelectorAll(`[id^="content-${lang}"]`).forEach(function(div) {
+            div.style.display = 'block';
+        });
+    }
+})
