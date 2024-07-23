2d522d3f22906e0b7ee794ba83c7263e59ca7357
Terry-mcgit <13884967603@163.com>
2024-07-23 22:23:01 +0800

    add debugger for transport.js

--- js/transport.js
+++ js/transport.js
@@ -1,9 +1,15 @@
 const userLang = navigator.language || navigator.userLanguage;
 const lanugages = ['cn','en','ru'];
+let debugFlag = 1;
 lanugages.forEach(lang => {
-    if (userLang.indexOf(`${lang}`) === 0) {
+    if (userLang.indexOf(`${lang}`) !== -1) {
         document.querySelectorAll(`[id^="content-${lang}"]`).forEach(function(div) {
             div.style.display = 'block';
+            console.log(`User Language Detected: ${lang}`);
+            debugFlag = 0;
         });
     }
 })
+if(debugFlag){
+    console.error(`Error: No Language Detected!`)
+}
