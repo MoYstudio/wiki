25db7fc13d0d174b1cf9a63151778fd1aecc8f66
Terry-mcgit <13884967603@163.com>
2024-07-23 22:32:49 +0800

    fixed transport.js!!!!!

--- js/transport.js
+++ js/transport.js
@@ -3,7 +3,7 @@ const lanugages = ['cn','en','ru'];
 let debugFlag = 1;
 lanugages.forEach(lang => {
     if (userLang.indexOf(`${lang}`) !== -1) {
-        document.querySelectorAll(`[id^="content-${lang}"]`).forEach(function(div) {
+        document.querySelectorAll(`#content-${lang}`).forEach(function(div) {
             div.style.display = 'block';
             console.log(`User Language Detected: ${lang}`);
             debugFlag = 0;
