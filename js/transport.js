6832957bc89e6bb91d40fa69485475d75a43d458
Terry-mcgit <13884967603@163.com>
2024-07-23 12:21:41 +0800

    aaa

--- js/transport.js
+++ js/transport.js
@@ -1,6 +1,5 @@
 const userLang = navigator.language || navigator.userLanguage;
 const lanugages = ['cn','en','ru'];
-const basePath = '../src/md';
 lanugages.forEach(lang => {
     if (userLang.indexOf(`${lang}`) === 0) {
         document.querySelectorAll(`[id^="content-${lang}"]`).forEach(function(div) {
