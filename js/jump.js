b374b5c4658ce42bdb7c7f4a98ca7da93f564e64
Terry-mcgit <13884967603@163.com>
2024-07-23 11:03:18 +0800

    js change

--- js/jump.js
+++ js/jump.js
@@ -1,26 +1,3 @@
-/*
-window.onload=function(){
-  var hash=window.location.hash;
-  if(!hash){
-    window.location.hash="home"
-  }
-}eee
-*/
-/*jump.js.v1
-$(function () {
-  common("div");
-
-  window.addEventListener("hashchange", function () {
-      common("div");
-  });
-
-  function common(dom) {
-      $(window.location.hash)
-          .slideDown()  
-          .siblings(dom)
-          .slideUp(); 
-  }
-});*/
 
 $(function () {
   common("div");
--- js/transport.js
+++ js/transport.js
@@ -1,16 +1,3 @@
-/*var userLang=navigator.language||navigator.userLanguage;
-if (userLang.indexOf('zh')===0){
-    var changeDivs=document.querySelectorAll("#content-cn");
-    changeDivs.forEach(function(div){
-        div.style.display='block';
-    })
-}
-else{
-    var changeDivs=document.querySelectorAll("#content-en");
-    changeDivs.forEach(function(div){
-        div.style.display='block';
-    })
-}*/
 const userLang = navigator.language || navigator.userLanguage;
 const lanugages = ['cn','en','ru'];
 const basePath = '../src/md';
